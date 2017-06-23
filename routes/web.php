6542db7d9b0d74fc9d4a7f8b034ad67a53fd6525
frederick sandalo <uscbsitric@gmail.com>
2017-06-23 09:45:44 +0000

    changed the credentials because I accidentally deleted the client in the oauth2server1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,36 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/redirect',
+		   function()
+		   {
+		   	$query = http_build_query(['client_id' => '3',
+		   							   'redirect_uri' => 'http://authorizationgrantclient1/callback',
+		   							   'response_type' => 'code',
+		   							   'scope' => ''
+		   							  ]
+		   							 );
+
+		   	return redirect('http://oauth2server1/oauth/authorize?' . $query);
+           }
+		  );
+
+Route::get('/callback',
+		   function(Illuminate\Http\Request $request)
+		   {
+		   	$http = new \GuzzleHttp\Client;
+		   	
+		   	$response = $http->post('http://oauth2server1/oauth/token', 
+		   			                ['form_params' => ['client_id' => '3',
+		   											   'client_secret' => 'CBgVDvofCj1ocT13wfT5VEaXY7qLt8r3kfh8l4Xu',
+		   											   'grant_type' => 'authorization_code',
+		   											   'redirect_uri' => 'http://authorizationgrantclient1/callback',
+		   											   'code' => $request->code,
+		   											  ],
+		   	                        ]
+		   						   );
+
+		   	return json_decode((string) $response->getBody(), true);
+           }
+		  );
