7b67f7c0d9c10f25ad5455f7e1629c49f6bb9215
Uchiha Madara <uchihamadara@Fredericks-MacBook-Pro.local>
2017-07-11 21:43:27 +0800

    solved the Implicit Grant Fiasco

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use GuzzleHttp\Cookie\CookieJar;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,7 +17,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+///////////// AUTHORIZATION GRANT TOKEN /////////////
 Route::get('/redirect',
+           function()
+           {
+             $query = http_build_query([
+        'client_id' => '1',
+        'redirect_uri' => 'http://authorizationgrantclient1/callback',
+        'response_type' => 'token',
+        'scope' => '',
+    ]);
+exit( var_dump('http://oauth2server1/oauth/authorize?'.$query) );
+    return redirect('http://oauth2server1/oauth/authorize?'.$query);
+
+    /*****
+    $query = http_build_query(['client_id' => '5',
+                               'redirect_uri' => 'http://authorizationgrantclient1/implicitGrantRedirectCallback',
+                               'response_type' => 'token',
+                               'scope' => '',
+                              ]
+                             );
+
+    return redirect('http://oauth2server1/oauth/authorize?'.$query);
+    *****/
+
+
+});
+
+Route::get('/callback',
+           function(Illuminate\Http\Request $request)
+           {
+             return var_dump($request);
+           }
+          );
+
+Route::get('/redirect2',
 		   function()
 		   {
 		   	$query = http_build_query(['client_id' => '4',
@@ -29,14 +66,14 @@ Route::get('/redirect',
            }
 		  );
 
-Route::get('/callback',
+Route::get('/callback2',
 		   function(Illuminate\Http\Request $request)
 		   {
 		   	$http = new \GuzzleHttp\Client;
-		   	
-		   	$response = $http->post('http://oauth2server1/oauth/token', 
+
+		   	$response = $http->post('http://oauth2server1/oauth/token',
 		   			                ['form_params' => ['client_id' => '4',
-		   											   'client_secret' => 'RfVD1eckCrEpGqIppokjMfoIswQsNcgkWZ17ERdr',
+		   											   'client_secret' => '3oNgT1iLvWw5JDXup8UfJ2apStMtk4OhgrvlGoId',
 		   											   'grant_type' => 'authorization_code',
 		   											   'redirect_uri' => 'http://authorizationgrantclient1/callback',
 		   											   'code' => $request->code,
@@ -52,23 +89,24 @@ Route::get('/test',
 		   function()
 		   {
 	         $http = new \GuzzleHttp\Client;
- 
-	         $response = $http->request('GET', 
-	         		                    'http://oauth2server1/api/user/1', 
-	         		                    ['headers' => ['Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjVjYjZlZGFmNzQ2MDIxZjNmODdmNDRjYWY3NTIwMmI0ZGQ3ZGJiMThmZGUwY2VkMWYzNWQxNWZmMWMyOWRlYTdjOTVlMWMzNmQ3MTM3NmMwIn0.eyJhdWQiOiI0IiwianRpIjoiNWNiNmVkYWY3NDYwMjFmM2Y4N2Y0NGNhZjc1MjAyYjRkZDdkYmIxOGZkZTBjZWQxZjM1ZDE1ZmYxYzI5ZGVhN2M5NWUxYzM2ZDcxMzc2YzAiLCJpYXQiOjE0OTg4NzM3MjMsIm5iZiI6MTQ5ODg3MzcyMywiZXhwIjoxNDk4ODczNzgzLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.d0WpTauGogy_52Kj5QU0pmRnBw7LGpmYFwsjZ5bw7SwHHarwyxBRHMOJay7fTBJdKinsONb-9rA5RftttlzNQrtr5VkHg_Xxn_0BW6hg9kR0LsFvq29ZqCsCXoDOao5VoVlfvb-3xy6oTfnhmc5dx_80dSpW8m31hCmlfFrgguCua5rC0hDw6Gd1Nkjg2mkFD-X26YGlPpSFWuNV3L5rHWnYop0-KId6IxqVOgoBHbpYTd0wFT3w8Pk5yoKRpDhJAUly465F-ZgPXt3giBPBidrtjWdVYc1NlZQqlYgxNXkT7pVVGsqCPF2w20aL6qr-FU9izoCmf4UGOeomFfOljLk_RMvbeSDmkdyVzp78pUl7LTjwYWGObjPrPw9XdyDnOanKowFZ5nY4CRSTlxARBJVmQZPRkuRAM---yRs576MqudVMoqiHG9BgKfw_KHjiCaTzpiC7TFrNEu1QNr-9os3s63N8mm9bs7fjzd6VNZvNW4w3AiOVLZC83jxLVPOl4Adi2aBho5OqKRREq3hql9lOZeOXtM7oQB4Mm4OZRM8vVLiO-7ywo3g0VRr1UHnRjr8h2fyF4k48F8Gg9tCrha6ofc8xjO3QS29r5J3FA-FqnBjBO_7BzWj-BCWt7aMsyVcGzi6FO5MKlkM6pUFpvVRQDblBJFAnatc7JG5OhTE'
+
+	         $response = $http->request('GET',
+	         		                    'http://oauth2server1/api/user/1',
+	         		                    ['headers' => ['Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImEzZjc3N2E4NTg1OTM4OWM4ZjE4ZGI2NTdmNDNlY2I1MmNhNzE5NmY1YzljYmYzNDc4YzE3YjQyN2NjMTU1ZDAwNWJkNjEzYjRmNWYxN2JmIn0.eyJhdWQiOiI0IiwianRpIjoiYTNmNzc3YTg1ODU5Mzg5YzhmMThkYjY1N2Y0M2VjYjUyY2E3MTk2ZjVjOWNiZjM0NzhjMTdiNDI3Y2MxNTVkMDA1YmQ2MTNiNGY1ZjE3YmYiLCJpYXQiOjE0OTk1MzAwMTYsIm5iZiI6MTQ5OTUzMDAxNiwiZXhwIjoxNTMxMDY2MDE2LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.boaHgsN751M47Bj-miJ338hSjKVd12K984DpwhClknyBnP56oFuLOZSW8XLWl93HEoOKu6dKOucoxOR1MQRfaCgJwyLBW01S0WeSVR4B9KMEY1dNUM7PfypUyaaLiAfyCuOxtUDnGGCK09Hq_d0kVqkgtuhGLjBajYhZxrD4e3zSWILrAPxF6Ue1NO8txKW2bni4beDiXv6_VcW3vvdDlOsn8NeU6kHtCLyhKmByyaFgJaqT0Ytvz1CMJw_MQj94L5T4lG5saZH7YBJ05tyz6XPufr2y0INss8UFx0bocsqGcb2m4ik-gNz7NxnFUO5qFZfQ_ZtgyO9VBrT2eDOkNutwIywtYM5yWPrTGdqnBes8yjYKC50SXLewP9CntJHDsYZ8NPtovgQ5HWmkUivpPDcoS5EVI1xE6deT3llkCTsHBCcXJMwtUhXcjudaWVlqaoDg60J19H3eJXM_m-NRf3jw8dO73UJV0k1fnjiwCEQPUC6iPtFwjxpzoXgYKbeMWZUqV4zuPE0P1yjlhwR1xkOlINWTJ7-sx_lgSKukrBs82sixTuqAMUfRXqv4VvEw5HKfqYOxGTd2nv85T0OuI9ZwD9eKFl1b-TX2cZxmn_wGxyTsbyhsZWzj7yEWaTJLrr0-MAUmj2oao5kUSBwJppujd_ZKfOiqfhFf20qGhVg'
 	         		                                  ]
 	         		                    ]
 	         		                   );
-	         /*
+	         /* DO NOT DELETE THESE PARTS, they are for reference and debugging purposes
 	         echo "<pre>";
 	         var_dump( ['statusCode'   => $response->getStatusCode(),
 	         		    'reasonPhrase' => $response->getReasonPhrase(),
      	         		'header'       => $response->getHeader('Content-Length'),
 	         		    'body'         => $response->getBody(),
 	         		    'bodyContents' => $response->getBody()->getContents()
-	                   ] 
+	                   ]
 	         		 );
 	         exit();
+	         *  DO NOT DELETE THESE PARTS, they are for reference and debugging purposes
 	         */
 	         /*
 	         $response = $http->get('http://oauth2server1/api/user/1',
@@ -82,14 +120,81 @@ Route::get('/test',
            }
 		  );
 
+
+Route::get('/test2',
+	       function()
+           {
+	         $http = new \GuzzleHttp\Client;
+		 /* // ?XDEBUG_SESSION_START=ECLIPSE_DBGP&KEY=14990601190393 */
+	         $cookieJar = CookieJar::fromArray(['XDEBUG_SESSION_START' => 'ECLIPSE_DBGP',
+                                                    'KEY' => '14990601190393'
+                                                   ],
+	         		                    'oauth2server1'
+	         		                  );
+
+	         $response = $http->request('GET',
+	         		            'http://oauth2server1/simpleTest',
+                                            ['cookies' => $cookieJar]
+	         		           );
+
+		     /*****
+		     *********************************************************************
+			 use GuzzleHttp\Cookie\CookieJar;
+
+			 $cookieJar = CookieJar::fromArray(['cookie_name' => 'cookie_value'],
+			 								   'example.com'
+			 								  );
+
+			 $client->request('GET', '/get', ['cookies' => $cookieJar]);
+		     *********************************************************************
+
+		     echo "<pre>";
+		     var_dump( ['statusCode'   => $response->getStatusCode(),
+			     		'reasonPhrase' => $response->getReasonPhrase(),
+			     		'header'       => $response->getHeader('Content-Length'),
+			     		'body'         => $response->getBody(),
+			     		'bodyContents' => $response->getBody()->getContents()
+		     		   ]
+		     		);
+		     exit();
+		     *****/
+
+		     $result = json_decode( (string) $response->getBody(), true );
+
+		     return $result;
+           }
+          );
+
+Route::get('/test3',
+           function()
+           {//exit('bati-a oy');
+             $ch = curl_init('http://oauth2server1/simpleTest');
+             curl_setopt($ch,   CURLOPT_COOKIE, "XDEBUG_SESSION_START=ECLIPSE_DBGP");
+             curl_exec($ch);
+           }
+          );
+
+Route::get('/test4',
+           function()
+           {
+             $ch = curl_init ();
+             curl_setopt ($ch, CURLOPT_URL, 'http://oauth2server1/simpleTest');
+             curl_setopt ($ch, CURLOPT_COOKIE, 'XDEBUG_SESSION=1');
+             curl_exec ($ch);
+           }
+          );
+
+Route::get('phpinfo', function(){ phpinfo(); });
+
+
 Route::get('/refreshToken',
 		   function()
 		   {
 		   	 $http = new GuzzleHttp\Client;
-		   	
-		   	 $response = $http->post('http://oauth2server1/oauth/token', 
+
+		   	 $response = $http->post('http://oauth2server1/oauth/token',
 		   	 		                  ['form_params' => ['grant_type' => 'refresh_token',
-									   					 'refresh_token' => 'QJ/TJ7VcLGub0EZVWpPEmrNf+526r38aCB2L4/zYXWql3ENT4y2LjbqAlg5hDEengZNyi4dE9OB/Cyk9VErHomhGWPhzCTY74chqLrLB5ATq5XTzdoghc92U1O9iDiP/AThcUPOxNx3Y3UXmz2mJ3UnGF2EnpbD3Q2imx4JICN601UHW+EiOSC/OHy5nq0eWPqNYWkT/EYL+uPYLg1j9rCf4laFQGwyiyDo1A+5jYtl+XWion7RevG2polZeW0F+QTT1ivxFiplhFmZkOYVmt8aFMxxDzz1rqp8qghGh+vbF4zquuI0/sI+5ZYow59buIQkaxS4fpjryQg5uZctjt28TxLxogNBIhuEjSbx2douTUY4p6Bnxeh7+o1fy++visFjsc3G8G15GogrmH7/p5rGPafRF93G4SJNO5z4407Docv+b1lBbX6d84WC2YVR636wyclUR3cGWs6meEMSp5Mu5P9okapi+IedQrtVWXPE2miJ629feYAVrqXz63hg0dRmmaYPubq3A/9YKCfxcDFYBr27CXg+R2/MIfwgzJHHA/nTossQppuA6E+nyDf0iAfZis804XKgC8KASSYvPUPmuOTjtiHULKHWMV339b29l048pulACww0WzyFSIl/iLBnZVdVplUW9Gu0c1ps50LyWKVdPtfL5XPLv6m+hiRk=',
+									   					 'refresh_token' => 'GCqZxY1k4ykdXWNfrWdB3GRxbjZWGYNfOfDQ3ZUQVFP0/EOypbGrNizQ/sh7ZlN3aqJw9/8iB5S4uV8fQSRSo6XBWL4/T2jSG8Zeltia1j1xxeCbcWsVE5hSR8Mx9m+f9dbEbpb4I4ISpsWV/f0DJaw4dQZxajBuXQqdHwNrk78LUUihW+wLvMJfdnBEQzC0JaT6sIm63Y5PWDkjXKDqv1QDcXobIcMy5CQkMEBUzfuAFMCyVoZW3FErkVz99zSGkstDR5nFTweOdTH2RgZ1NANNIbL7m3e8JBJ+pg5QP9UhWxpfVF3yrHFY1W6XEYAfKBWGg2CiKXIQJQTaTvKj32VJXwc5bEzzxdQ4/2RA3Ovp4A2/6nPfApE72kulBe3ypruvQMbs6Q5LdkYy/FqBQ6vBA+KV0sMwLEBISTUNuhWQssxNUx1XEUvRW8WJgPQx62heDzB2aLYn2NqlfjS+iO38DpCmGP9Bmvwkgu3XmaX4LdX1tLzWN02HuffTjZ464d3V36kLM7xh0FLkjuc88XOLr8WkydGVxC/NK3U8HyoPfxdSEnxArpOHyXXJhOIpXdXp0mmFavpg/4H5o1LdS5m+hLnuQAJNo7EOcJWT+GcF1yM8X7/mwORZhSbDXF7CjPN5xMF2DSfvjg5Rbl+YmInWMM/OoZDfi+o0ROdDHKw=',
 									   					 'client_id' => '4',
 									   					 'client_secret' => 'RfVD1eckCrEpGqIppokjMfoIswQsNcgkWZ17ERdr',
 									   					 'scope' => '',
@@ -100,3 +205,74 @@ Route::get('/refreshToken',
 		   	return json_decode((string) $response->getBody(), true);
            }
 		  );
+///////////// AUTHORIZATION GRANT TOKEN /////////////
+
+
+///////////// PASSWORD GRANT TOKEN /////////////
+Route::get('/passwordTokenGrantTest',
+		   function()
+		   {
+		   	 $http = new GuzzleHttp\Client;
+
+		   	 $response = $http->post('http://oauth2server1/oauth/token',
+    		   	 		                 ['form_params' => ['grant_type' => 'password',
+                    									   					  'client_id' => '3',
+                    									   					  'client_secret' => 'WYveOsogWzD3t4RehAPctuL1EWL4uZzpiM51XAN9',
+                    									   					  'username' => 'user1@somewhere.com',
+                    									   					  'password' => 'user1',
+                    									   					  'scope' => '',
+                    									   				   ],
+    		   							         ]
+		   	 						            );
+
+		   	return json_decode((string) $response->getBody(), true);
+           }
+          );
+
+Route::get('/passwordGrantClientRedirect',
+           function()
+           {
+             return 'this part not complete yet';
+           }
+          );
+///////////// PASSWORD GRANT TOKEN /////////////
+
+
+///////////// IMPLICIT GRANT TOKEN /////////////
+Route::get('/implicitGrantRedirect',
+           function()
+           {
+             $query = http_build_query(['client_id' => '5',
+                                        'redirect_uri' => 'http://authorizationgrantclient1/implicitGrantRedirectCallback',
+                                        'response_type' => 'token',
+                                        'scope' => '',
+                                       ]
+                                      );
+
+             return redirect('http://oauth2server1/oauth/authorize?'.$query);
+           }
+          );
+
+Route::get('/implicitGrantRedirectCallback',
+           function(Illuminate\Http\Request $request)
+           {
+             /*****
+             $http = new \GuzzleHttp\Client;
+
+          	 $response = $http->post('http://oauth2server1/oauth/token',
+          		   			               ['form_params' => ['client_id' => '5',
+          		   											                  'client_secret' => 'sLbwI1zFImcvsunqSYpdq6WkIqnvNzTuCADAohuN',
+          		   											                  'grant_type' => 'authorization_code',
+          		   											                  'redirect_uri' => 'http://authorizationgrantclient1/callback',
+          		   											                  'code' => $request->code,
+          		   											                 ],
+          		   	                   ]
+          		   						        );
+
+          	 return json_decode((string) $response->getBody(), true);
+             *****/
+
+             return var_dump($request);
+          }
+         );
+///////////// IMPLICIT GRANT TOKEN /////////////
